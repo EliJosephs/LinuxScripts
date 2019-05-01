29181c1355e0a58f75c0f238ac938c01cf0c154f
Eli Josephs <focob17g@linuxa1.engr.colostate.edu>
2019-05-01 02:09:38 -0600

    Comment edits.

--- FPS.sh
+++ FPS.sh
@@ -1,16 +1,17 @@
 #!/bin/ksh93
-# This file provides theoretical "calculations per second" of a linux system
+
+# This file provides theoretical "calculations per second" of a linux system.
 # 
 # How it works:
 #
 # First, the time is taken before any calculations are done. 
-# Second, a loop is run which performs a basic addition calculation to variable "x" starting at 0
-# Third, when the loop has been run for the step size (seconds), it ends
-# Fourth, the FPS is calculated from the resulting "x" and the step size in seconds
+# Second, a loop is run which performs a basic addition calculation to variable "x" starting at 0.
+# Third, when the loop has been run for the step size (seconds), it ends.
+# Fourth, the FPS is calculated from the resulting "x" and the step size in seconds.
 #
 # This is not a great indicator of how fast your system can actually perform calculations, however, it may be helpful for monitoring changes in performance. 
 #
-# Written by Eli Josephs, 5-1-19, remote server via vim
+# Written by Eli Josephs, 5-1-19, remote server via vim.
 
 maxSpeed=0.05 # Arbitrary value, but beyond this, the output numbers change so quick it's pointless, and the "sample" size becomes so small that tiny errors result in large changes to the output. For example, the FPS reads as either 2000 or 0, which we know is incorrect. The loop can't run long enough to count an appreciable amount of times per time interval. Even at this number, especially on sloer machines, the resulting FPS will have a high error. Longer intervals will result in less error. 
 
